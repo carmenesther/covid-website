74c3af96c1c82db73560ce0cde392f26293aaa59
manucaro <manuel.caro.mendez@gmail.com>
2021-06-09 18:44:40 +0200

    Mover a app_server

--- express-app/app_api/routes/index.js
+++ express-app/app_api/routes/index.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Health zone pages */
+router.get('/health-zones', {title: "Por definir el controlador de health zones"});
+
+
 module.exports = router;
