a04ef19c1ab8a95e36bdfeebc8b6fe5ff593ed55
manucaro <manuel.caro.mendez@gmail.com>
2021-06-09 19:02:16 +0200

    Modelo Death

--- express-app/app_api/models/deaths.js
+++ express-app/app_api/models/deaths.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 
 const deathSchema = new mongoose.Schema({
-    'Covid-19': String,
-    'Mes de defunción': String,
-    'Sexo': String,
-    'Edad': String,
-    'Total': String
+    covid19: {
+        type: String,
+        required: true
+    },
+    mes_de_defuncion: {
+        type: String,
+        required: true
+    },
+    sexo: {
+        type: String,
+        required: true
+    },
+    edad: {
+        type: String,
+        required: true
+    },
+    total: {
+        type: Number,
+        required: true
+    }
 });
 
 mongoose.model('Death', deathSchema);
