7c1c837e51c560b4c0708d675d6b5fcb007e3bf5
manucaro <manuel.caro.mendez@gmail.com>
2021-06-11 19:43:21 +0200

    Añado nuevo modelo y métodos al dbservice

--- angular-app/src/app/services/db.service.ts
+++ angular-app/src/app/services/db.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { VaccinationModel } from 'src/app/models/vaccination.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,20 +12,36 @@ export class DbService {
   
   constructor(private http: HttpClient) { }
 
-  getAllHealthZones() {
-    return this.http.get(`${this.apiUrl}/health-zones`);
-  }
-
-  getHealthZone(id) {
-    return this.http.get(`${this.apiUrl}/health-zones/${id}`);
-  }
-
   getAllDeaths() {
-    return this.http.get(`${this.apiUrl}/deaths`);
+    return this.http.get(`${this.apiUrl}/api/deaths`);
   }
 
   getDeath(id) {
-    return this.http.get(`${this.apiUrl}/deaths/${id}`);
+    return this.http.get(`${this.apiUrl}/api/deaths/${id}`);
+  }
+
+  getDeathsByGenre(genre: string) {
+    return this.http.get(`${this.apiUrl}/api/deaths-genre/${genre}`);
+  }
+
+  getCountGenre() {
+    return this.http.get(`${this.apiUrl}/api/deaths-count-genre`);
+  }
+
+  getAllHealthZones() {
+    return this.http.get(`${this.apiUrl}/api/health-zones`);
+  }
+
+  getHealthZone(id) {
+    return this.http.get(`${this.apiUrl}/api/health-zones/${id}`);
+  }
+
+  getAllVaccination() {
+    return this.http.get(`${this.apiUrl}/api/vaccination`);
+  }
+
+  addVaccination(vaccination: VaccinationModel) {
+    return this.http.post(`${this.apiUrl}/api/vaccination/`, vaccination);
   }
 
 }
