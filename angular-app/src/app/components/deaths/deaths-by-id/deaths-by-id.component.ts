4c2195945c14ad25af3963895e458d9f887f0f8f
Carmen Fernandez <fernandezcarmen1997@gmail.com>
2021-06-13 14:49:15 +0200

    añadida la vista de muerte por id

--- angular-app/src/app/components/deaths/deaths-by-id/deaths-by-id.component.ts
+++ angular-app/src/app/components/deaths/deaths-by-id/deaths-by-id.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DbService } from 'src/app/services/db.service';
+import { faFemale, faMale, faVenusMars} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-deaths-by-id',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeathsByIdComponent implements OnInit {
 
-  constructor() { }
+  deathId; 
+  death: any = [];
+
+  wait: boolean = true;
+
+  faFemale = faFemale;
+  faMale = faMale;
+  faBothGenders = faVenusMars;
+
+  constructor(private db: DbService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.deathId = this.route.snapshot.paramMap.get('id');
+    this.getDeathById(this.deathId);
+  }
+
+  getDeathById(id) {
+    this.db.getDeath(id).subscribe(data => {
+      this.death = data;
+      console.log(this.death);
+      this.wait = false;
+    })
   }
 
 }
